e7eaf1535dfaf8bca7aa9dbeafd9a8bc2175c10a
Chris Cornelison <chris.cornelison@pioneer.com>
2017-05-30 20:11:24 +0000

    setup git and upgrade vim

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# setup git
+git config --global user.name "Chris Cornelison"
+git config --global user.email "chris.cornelison@pioneer.com"
+
 # upgrade to Vim 8.0
 sudo add-apt-repository -y ppa:jonathonf/vim
 sudo apt -y update
