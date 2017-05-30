04564ea23b744bcd74d65158ca4c2fdb3355eaa3
Chris Cornelison <chris.cornelison@pioneer.com>
2017-05-30 21:37:29 +0000

    updates

--- install.sh
+++ install.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
-## setup git
-#git config --global user.name "Chris Cornelison"
-#git config --global user.email "chris.cornelison@pioneer.com"
-#
-## upgrade and configure Vim 8.0
-#sudo add-apt-repository -y ppa:jonathonf/vim
-#sudo apt -y update
-#sudo apt -y install vim
-#git clone --recursive https://github.com/chriscnc/dotfiles.git
-#ln -s dotfiles/vim .vim
-#ln -s dotfiles/vimrc .vimrc
+# setup git
+git config --global user.name "Chris Cornelison"
+git config --global user.email "chris.cornelison@pioneer.com"
 
-## install misc utilities
-##sudo apt -y install tree
+# upgrade and configure Vim 8.0
+sudo add-apt-repository -y ppa:jonathonf/vim
+sudo apt -y update
+sudo apt -y install vim
+git clone --recursive https://github.com/chriscnc/dotfiles.git
+ln -s dotfiles/vim .vim
+ln -s dotfiles/vimrc .vimrc
 
-## JDK 8
-#sudo apt -y install openjdk-8-jdk-headless
+# install misc utilities
+sudo apt -y install tree awscli
 
-## Maven
-#sudo apt -y install maven
+# JDK 8
+sudo apt -y install openjdk-8-jdk-headless
 
-## Spark
-#wget https://d3kbcqa49mib13.cloudfront.net/spark-2.1.1-bin-hadoop2.7.tgz
-#sudo tar zxvf spark-2.1.1-bin-hadoop2.7.tgz -C /opt
-#sudo ln -fs spark-2.1.1-bin-hadoop2.7 /opt/spark
-#echo '# Spark' >> .bashrc
-#echo 'export SPARK_HOME=/opt/spark' >> .bashrc
-#echo 'export PATH=$PATH:$SPARK_HOME/bin' >> .bashrc
+# Maven
+sudo apt -y install maven
+
+# Spark
+wget https://d3kbcqa49mib13.cloudfront.net/spark-2.1.1-bin-hadoop2.7.tgz
+sudo tar zxvf spark-2.1.1-bin-hadoop2.7.tgz -C /opt
+sudo ln -fs spark-2.1.1-bin-hadoop2.7 /opt/spark
+echo '# Spark' >> .bashrc
+echo 'export SPARK_HOME=/opt/spark' >> .bashrc
+echo 'export PATH=$PATH:$SPARK_HOME/bin' >> .bashrc
 
 # install Pyenv
 sudo apt -y install make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils
@@ -38,4 +38,7 @@ echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> .bashrc
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 git clone https://github.com/pyenv/pyenv-virtualenv.git .pyenv/plugins/pyenv-virtualenv
 echo 'eval "$(pyenv virtualenv-init -)"' >> .bashrc 
+pyenv install 3.6.1
+pyenv global 3.6.1
+
 
